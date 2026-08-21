64bd9d6e0d772036be022d7f264fa167a0c014f0
MeaksVR <mattyrocks718@gmail.com>
2026-08-21 15:43:01 -0400

    Delete site.js

--- site.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
- const header=document.querySelector('header');
- if(header)header.innerHTML=`<div class="wrap nav"><a class="logo" href="index.html">MEKO <b>GAMES VR</b></a><nav class="links"><a href="index.html">Home</a><a href="games.html">Games</a><a href="updates.html">Updates</a><a href="blog.html">Blog</a><a href="about.html">About</a><a href="${SITE.youtube}" target="_blank">YouTube ↗</a></nav><button class="nav-btn" onclick="alert('Use the navigation links on desktop, or tap a page link from the address bar.')">☰</button></div>`;
- const footer=document.querySelector('footer');
- if(footer)footer.innerHTML=`<div class="wrap footer"><span>© 2026 Meko Games VR. All rights reserved.</span><div class="footer-links"><a href="${SITE.youtube}" target="_blank">YouTube</a><a href="${SITE.discord}" target="_blank">Discord</a></div></div>`;
-});
